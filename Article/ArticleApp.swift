6d25d4007664609a78473ebccd075d3681029995
Amit Tandel <46502349+itsmrtandel@users.noreply.github.com>
2023-04-13 21:14:29 -0700

    Add files via upload

---
+++ Article/ArticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleApp.swift
+//  Article
+//
+//  Created by Amit Tandel on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ArticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
